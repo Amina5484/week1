fdfaaa43d50de7285e53e11e4c23d78ee34377a4
Amina5484 <147622661+Amina5484@users.noreply.github.com>
2023-11-04 21:59:25 +0300

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,13 +1,7 @@
 class Teacher extends Person {
-    public String getSpecialization() {
-        return specialization;
-    }
+    
 
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    private String specialization;
+     String specialization;
 
     public Teacher( String name, int age, boolean parent_permission,String specialization) {
         super( name, age, parent_permission);

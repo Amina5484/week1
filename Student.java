ddafea6817dc7f043672b339907d603b1531847b
userName <aminamohammed10004@gmail.com>
2023-11-11 21:37:23 +0300

    complete project_2

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 class Student extends Person {
     private String classroom;
-
+//here is student class
     public Student( String name, int age, boolean parent_permission,String classroom) {
         super( name, age,parent_permission);
         this.classroom = classroom;

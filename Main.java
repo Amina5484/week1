b607f8604785d53dd7505d238e41a253e58b1a49
mengaww2ya <mengaww2ya@gmail.com>
2023-12-02 06:20:59 -0800

    completed week 5

--- Main.java
+++ Main.java
@@ -1,55 +1,55 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        App app = new App();
-
-        while (true) {
-            printMenu();
+        boolean quit = false;
+        App library = new App();
+        while (!quit) {
+            System.out.println("*************library**************");
+            System.out.println("Choose an option:");
+            System.out.println("1. Create a person");
+            System.out.println("2. Create a book");
+            System.out.println("3. List all people");
+            System.out.println("4. List all books");
+            System.out.println("5. Create a rental");
+            System.out.println("6. List rentals for a person");
+            System.out.println("7. Quit");
+            System.out.println("*************end**************");
             int option = scanner.nextInt();
-            scanner.nextLine(); // Consume newline character
+            scanner.nextLine();
 
             switch (option) {
                 case 1:
-                    app.listAllBooks();
+                    System.out.println("*************Create person*************");
+                    library.createPerson();
                     break;
                 case 2:
-                    app.listAllPeople();
+                    System.out.println("*************Create Book**************");
+                    library.createBook();
                     break;
                 case 3:
-                    app.createPerson();
+                    System.out.println("*************list of people**************");
+                    library.listAllPeople();
                     break;
                 case 4:
-                    app.createBook();
+                    System.out.println("*************list Book**************");
+                    library.listAllBooks();
                     break;
                 case 5:
-                    app.createRental();
+                    System.out.println("*************Create rental**************");
+                    library.createRental();
                     break;
                 case 6:
-                    app.listRentalsForPerson();
+                    System.out.println("*************list of Rental**************");
+                    library.listRentalsForPerson();
                     break;
                 case 7:
-                    System.out.println("Exiting the program. Goodbye!");
-                    System.exit(0);
+                    quit = true;
+                    System.out.println("*************Thanks for using **************");
                     break;
                 default:
                     System.out.println("Invalid option");
             }
         }
-    }
-
-    private static void printMenu() {
-        System.out.println("******* Library ****************");
-        System.out.println("Choose an option:");
-        System.out.println("1. List all books");
-        System.out.println("2. List all people");
-        System.out.println("3. Create a person");
-        System.out.println("4. Create a book");
-        System.out.println("5. Create a rental");
-        System.out.println("6. List rentals for a person");
-        System.out.println("7. Quit");
-        System.out.println("************************************");
-    }
-}
--- Student.java
+++ Student.java
@@ -1,16 +1,18 @@
 class Student extends Person {
     private String classroom;
 
-    //here is student class
-    public Student(String name, int age,  int id,boolean parent_permission,String classroom) {
-        super(name, age, id,parent_permission);
+
+    public Student(String name, int age,boolean parent_permission,String classroom) {
+        super(name, age, parent_permission);
+        this.classroom = classroom;
 
     }
     public String  getClassroom() {
         return classroom;
     }
 
-    public void setClassroom(String classroom) {
+    public void setClassroom(String classroom)
+    {
         this.classroom = classroom;
     }
 }
